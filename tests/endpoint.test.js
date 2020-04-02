f9753b5906c47e7fd9432270ecc63b866c5d207b
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2020-04-02 16:04:40 +0530

    added endpoint-tests

---
+++ tests/endpoint.test.js
@@ -0,0 +1,50 @@
+const request = require('supertest')
+const app = require('../app')
+
+describe('Perform successful operations', () => {
+
+    it('should pass', async () => {
+        const res = await request(app)
+            .get('/api/v1/index/pass');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body.error).toEqual(false);
+        expect(res.body.message).toEqual('GET request served');
+        expect(res.body.data).toEqual(null);
+    });
+
+    it('should pass', async () => {
+        const res = await request(app)
+            .post('/api/v1/index/pass');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body.error).toEqual(false);
+        expect(res.body.message).toEqual('POST request served');
+        expect(res.body.data).toEqual(null);
+    });
+
+});
+
+describe('Perform unsuccessful operations', () => {
+
+    it('should not pass', async () => {
+        const res = await request(app)
+            .get('/api/v1/index/fail');
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.success).toEqual(false);
+        expect(res.body.error).toEqual(true);
+        expect(res.body.message).toEqual('GET request not served');
+        expect(res.body.data).toEqual(null);
+    });
+
+    it('should not pass', async () => {
+        const res = await request(app)
+            .post('/api/v1/index/fail');
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.success).toEqual(false);
+        expect(res.body.error).toEqual(true);
+        expect(res.body.message).toEqual('POST request not served');
+        expect(res.body.data).toEqual(null);
+    });
+
+});
