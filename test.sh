74aebc11c00d9e18cace8c8ad4873f3406afc1d5
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2020-04-04 01:34:01 +0530

    removed demo test report

--- test.sh
+++
@@ -1,8 +0,0 @@
-echo 'Running test docker image'
-docker run --name test-image-container demo-app-test
-echo 'Successfully ran test docker image'
-
-echo 'Removing test docker image'
-docker container stop test-image-container
-docker rmi demo-app-test
-echo 'Removed test docker image'
