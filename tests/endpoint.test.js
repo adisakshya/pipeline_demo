88eabff06f063ec5755b9cc1745335084e2e0a73
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2020-04-07 14:08:27 +0530

    merged test-suites

--- tests/endpoint.test.js
+++ tests/endpoint.test.js
@@ -1,6 +1,18 @@
 const request = require('supertest')
 const app = require('../app')
 
+describe('Ping', () => {
+    it('should ping the server', async () => {
+      const res = await request(app)
+        .get('/api/v1/ping');
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.success).toEqual(true);
+      expect(res.body.error).toEqual(false);
+      expect(res.body.message).toEqual('pong');
+      expect(res.body.data).toEqual(null);
+    });
+});
+
 describe('Perform successful operations', () => {
 
     it('should pass', async () => {
