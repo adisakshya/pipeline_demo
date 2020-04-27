cbf8863d32401236d1279779d69f70a80d061e0c
adisakshya <adisakshya98@gmail.com>
2020-04-27 17:54:35 +0530

    added script to start ssh-service nodes

---
+++ ansible/kubeup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Prune docker containers
+echo '> Pruning docker containers'
+docker container prune -f
+
+# Setup Kubernetes Master Node
+echo '> Setting up Kubernets Master node'
+docker run -d -p 2223:22 \
+    --name K8Master \
+    -v "/$(pwd)/keys/ansible.pub:/home/ubuntu/.ssh/authorized_keys" \
+    adisakshya/ssh-service:latest \
+    && ssh-copy-id -f  -i keys/ansible root@192.168.99.101 -p 2223
+
+# Setup Kubernetes Worker Node 1
+echo '> Setting up Kubernets Worker node 1'
+docker run -d -p 2224:22 \
+    --name k8Worker1 \
+    -v "/$(pwd)/keys/ansible.pub:/home/ubuntu/.ssh/authorized_keys" \
+    adisakshya/ssh-service:latest \
+    && ssh-copy-id -f  -i keys/ansible root@192.168.99.101 -p 2224
