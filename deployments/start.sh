ac89acae103052c90e1f07376291dbf1d9a2bbde
adisakshya <adisakshya98@gmail.com>
2020-05-02 21:00:54 +0530

    updated playbooks

--- deployments/start.sh
+++ deployments/start.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
 
-# Ping all hosts
-echo '> Pinging all hosts'
-docker run --rm -it \
-    -v "/$(pwd)/keys:/root/.ssh" \
-    -v "/$(pwd)/hosts:/etc/ansible" \
-    adisakshya/ansible:latest \
-    ansible all -m ping -i //etc/ansible/hosts.yml
-
-
+VERSION='1.0.3'
 ENV='production'
 
 # Run master playbook
 echo '> Runing master playbook'
 docker run --rm -it \
     -v "/$(pwd)/keys:/root/.ssh" \
-    -v "/$(pwd)/hosts:/etc/ansible" \
+    -v "/$(pwd)/ansible:/etc/ansible" \
     -v "/$(pwd):/ansible" \
     adisakshya/ansible:latest \
-    ansible-playbook site.yml --extra-vars "env=$ENV" -i //etc/ansible/hosts.yml 
+    ansible-playbook site.yml --extra-vars "env=$ENV version=$VERSION" -i '//etc/ansible/hosts.yml'
