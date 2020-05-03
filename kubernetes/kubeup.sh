5bf63c5d95a11f864f226fcae58734c247290828
adisakshya <adisakshya98@gmail.com>
2020-05-03 13:28:45 +0530

    removed prerequisite playbook from /kubernetes

--- kubernetes/kubeup.sh
+++ kubernetes/kubeup.sh
@@ -5,12 +5,3 @@ kind delete cluster
 
 # Create new kind cluster from config file
 kind create cluster --config=config/cluster-config.yml
-
-# Run master playbook
-echo '> Runing master playbook'
-docker run --rm -it \
-    -v "/$(pwd)/keys:/root/.ssh" \
-    -v "/$(pwd)/ansible:/etc/ansible" \
-    -v "/$(pwd):/ansible" \
-    adisakshya/ansible:latest \
-    ansible-playbook prerequisites.yml -i '//etc/ansible/hosts.yml'
