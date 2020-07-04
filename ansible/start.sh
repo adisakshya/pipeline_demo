7d590bb7a9252818193fede6f1f3a6468168080b
adisakshya <adisakshya@triveous.com>
2020-07-04 22:01:50 +0530

    renamed /deployments to /ansible

--- ansible/start.sh
+++ ansible/start.sh
@@ -6,8 +6,7 @@ ENV='production'
 # Run master playbook
 echo '> Runing master playbook'
 docker run --rm -it \
-    -v "/$(pwd)/keys:/root/.ssh" \
-    -v "/$(pwd)/ansible:/etc/ansible" \
+    -v "/$(pwd)/config:/etc/ansible" \
     -v "/$(pwd):/ansible" \
     adisakshya/ansible:latest \
     ansible-playbook site.yml --extra-vars "env=$ENV version=$VERSION" -i '//etc/ansible/hosts.yml'
